5eb7d1468ab88c2cfab82c85acc1242fd501e516
Gaia Picorelli <gaiapicorelli@gmail.com>
2019-08-26 19:08:19 +0200

    animazione javascript funziona!!

--- public/js/animazioneCompetenze.js
+++ public/js/animazioneCompetenze.js
@@ -1,35 +1,38 @@
-// get the element to animate
-var element = document.getElementById('point');
-var elementHeight = element.clientHeight;
+// ANIMAZIONE 1
 
 // listen for scroll event and call animate function
-document.addEventListener('scroll', animate);
+document.addEventListener('scroll', function () { animate("point"); });
+document.addEventListener('scroll', function () { animate("point2"); });
 
 // check if element is in view
-function inView() {
+function inView(element) {
+
+  var elementHeight = element.clientHeight;
   // get window height
   var windowHeight = window.innerHeight;
   // get number of pixels that the document is scrolled
   var scrollY = window.scrollY || window.pageYOffset;
-  
+
   // get current scroll position (distance from the top of the page to the bottom of the current viewport)
   var scrollPosition = scrollY + windowHeight;
   // get element position (distance from the top of the page to the bottom of the element)
   var elementPosition = element.getBoundingClientRect().top + scrollY + elementHeight;
-  
+
   // is scroll position greater than element position? (is element in view?)
   if (scrollPosition > elementPosition) {
     return true;
   }
-  
+
   return false;
 }
 
 // animate element when it is in view
-function animate() {
+function animate(elementId) {
+  // get the element to animate
+  var element = document.getElementById(elementId);
   // is element in view?
-  if (inView()) {
-      // element is in view, add class to element
-      element.classList.add('animate');
+  if (inView(element)) {
+    // element is in view, add class to element
+    element.classList.add('animate');
   }
 }
