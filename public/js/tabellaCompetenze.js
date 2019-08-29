5d4342182f4f83b9e9deea83b2a17b8a1c856104
Gaia Picorelli <gaiapicorelli@gmail.com>
2019-08-29 14:13:07 +0200

    più o meno sistemata la prima parte di sudime

--- public/js/tabellaCompetenze.js
+++ public/js/tabellaCompetenze.js
@@ -1,5 +1,5 @@
-document.getElementById("CompetenzeProfessionali").classList.add("inactive");
-document.getElementById("CompetenzePersonali").classList.add("inactive");
+document.getElementById("competenze-professionali").classList.add("inactive");
+document.getElementById("competenze-personali").classList.add("inactive");
 
 function toggleBody(element) {
     if (document.getElementById(element).classList.contains("inactive")) {
