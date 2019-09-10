290dadce22a6fa862485a37f4c79b9cbfa9ea038
Matteo Sovilla <teo.sovi@gmail.com>
2019-09-10 19:30:52 +0200

    fixata homepage

--- js/animazioneHeader.js
+++ js/animazioneHeader.js
@@ -1,15 +1,9 @@
-var mq = window.matchMedia("(max-width: 948px)");
-if (mq.matches) {
-    document.getElementById("nav").classList.add("closed-header");
-}
-
 function toggleHeader() {
-    var nav = document.getElementById("nav");
+    const nav = document.getElementById("nav");
     if (nav.classList.contains("closed-header")) {
         nav.classList.remove("closed-header");
         nav.classList.add("open-header");
-    }
-    else if (nav.classList.contains("open-header")) {
+    } else if (nav.classList.contains("open-header")) {
         nav.classList.remove("open-header");
         nav.classList.add("closed-header");
     }
